d04d3b9e9d928b97a79effc68f1a721f16376f57
Hashem <hashemp206@yahoo.com>
2021-04-04 20:43:15 +0400

    Create PhotoGallery main entrance module
    MainApp is main entrance of the app and it only uses composed iOS modules and run them

---
+++ PhotoGalleryiOS/PhotoGalleryiOS/Composers/GalleryUIComposer.swift
@@ -0,0 +1,19 @@
+//
+//  Composers.swift
+//  PhotoGalleryiOS
+//
+//  Created by Hashem Aboonajmi on 4/4/21.
+//
+
+import UIKit
+import PhotoGalleryFeature
+
+public final class GalleryUIComposer {
+    private init() {}
+    
+    public static func photoGalleryComposedWith(photoLoader: PhotoLoader, photoSaver: PhotoSaver) -> PhotoGalleryViewController {
+        
+        return PhotoGalleryViewController()
+    }
+    
+}
