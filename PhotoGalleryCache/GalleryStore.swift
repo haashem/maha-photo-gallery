96e47edb098af0fbae9fbee99cdfc06e8b81e3ba
Hashem <hashemp206@yahoo.com>
2021-04-04 14:13:05 +0400

    Define an interface for loading photos

--- PhotoGalleryCache/GalleryStore.swift
+++ PhotoGalleryCache/GalleryStore.swift
@@ -12,5 +12,9 @@ public protocol GalleryStore {
     typealias InsertionResult = Result<Void, Error>
     typealias InsertionCompletion = (InsertionResult) -> Void
     
+    typealias RetrievalResult = Result<[LocalPhoto]?, Error>
+    typealias RetrievalCompletion = (RetrievalResult) -> Void
+    
     func insert(_ feed: LocalPhoto, insertionCompletion: @escaping InsertionCompletion)
+    func retrieve(completion: @escaping RetrievalCompletion)
 }
