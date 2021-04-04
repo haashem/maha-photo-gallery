397e6392c743f2730543174f2a37d12e7ae1ccff
Hashem <hashemp206@yahoo.com>
2021-04-04 14:07:32 +0400

    Add GalleryCache module
    It will handle loading and saving photos locally

---
+++ PhotoGalleryCacheTests/PhotoGalleryCacheTests.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoGalleryCacheTests.swift
+//  PhotoGalleryCacheTests
+//
+//  Created by Hashem Aboonajmi on 4/4/21.
+//
+
+import XCTest
+import PhotoGalleryCache
+
+class PhotoGalleryCacheTests: XCTestCase {
+
+    
+}
